69fe4b244a8edde1550a20af5480f48f4285dc09
Joseph Wang <wang.jo@husky.neu.edu>
2017-10-12 21:00:50 -0400

    Initial Push of Source Files
    This is the initial push of the files from the source directory of the
    weather app to Github.

---
+++ main.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, jsonify
+import requests
+import json
+import os
+
+# Start the main app
+app = Flask(__name__)
+# Set the directory to retrieve static files from
+app.static_folder = 'static'
+
+# Variables to use for app
+location = None
+weather_api_key = "79b57a44308eea0e1f732061cf675137"
+
+# Methods
+def getLocationIP():
+	url = 'http://freegeoip.net/json'
+	req = requests.get(url)
+	res = req.json()
+	return res
+
+def cleanAPI(_json):
+    # if exception is thrown here, then return error message
+    try:
+        cleaned = {}
+        cleaned['name'] = _json['name']
+        cleaned['temp'] = {
+            'current': int(_json['main']['temp']),
+            'min':  int(_json['main']['temp_min']),
+            'max':  int(_json['main']['temp_max'])
+        }
+        cleaned['features'] = _json['weather']
+        cleaned['humidity'] = _json['main']['humidity']
+    except:
+        return {"error": "bad location"}
+    # wind speed (meter/sec) and direction
+    try:
+        cleaned['wind'] = _json['wind']
+    except:
+        cleaned['wind'] = 0
+    # last 3hr rain volume
+    try:
+        cleaned['rain'] = _json['rain']
+    except:
+        cleaned['rain'] = 0
+    # last 3hr rain volume
+    try:
+        cleaned['snow'] = _json['snow']
+    except:
+        cleaned['snow'] = 0
+    # cloud coverage (%)
+    try:
+        cleaned['clouds'] = _json['clouds']['all']
+    except:
+        cleaned['clouds'] = "none"
+    return cleaned
+
+def requestWeather(_data):
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    payload = _data
+    payload['APPID'] = weather_api_key
+    req = requests.get(url, params=payload)
+    res = cleanAPI(req.json())
+    return res
+
+# Routes
+@app.route('/')
+def landing():
+    location = getLocationIP()
+    # render the landing page template here
+    return render_template('landing.html', location=location)
+
+@app.route('/weather', methods=['GET'])
+def weatherAPI():
+    _type = request.args.get('type')
+    if _type == 'zipcode':
+        _data = { 'zip': request.args.get('zip') }
+        _result = requestWeather(_data)
+    else:
+        _geo = getLocationIP()
+        _data = { 
+            'lat': _geo['latitude'],
+            'lon': _geo['longitude'] 
+        }
+        _result = requestWeather(_data)
+    return jsonify(_result)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/script.js
@@ -0,0 +1,163 @@
+// The parent Vue instance
+var vm = new Vue({
+	el: "#app",
+	data: {
+		weather: {},
+		day: null,
+		launched: false,
+		zip: null,
+		unit: 'F',
+		error: null
+	},
+	methods: {
+		// --------- CONTROL METHODS BELOW ---------
+
+		// Launch to view weather data (abstracted here for to allow for future changes)
+		launch: function() {
+			this.launched = true;
+		},
+		// Return to main screen
+		goBack: function() {
+			this.launched = false;
+		},
+		setUnit: function(unit) {
+			this.unit = unit;
+		},
+
+		// --------- DATA METHODS BELOW ---------
+
+		// Helper method to get weather - zipcode
+		weatherZip: function() {
+			Vue.nextTick(function(){
+				$('#_launch input[name="zip-code"]').parent().removeClass("error");
+			});
+			var data = { type: 'zipcode', zip: this.zip };
+        	this.weatherAPI(data);
+        	// reset zip input
+        	this.zip = "";
+		},
+		// Helper method to get weather - "my location"
+		weatherLoc: function() {
+		    var data = { type: 'geolocation' };
+        	this.weatherAPI(data);
+		},
+		// Parent method to get weather
+		weatherAPI: function(data) {
+			var self = this;
+        	Vue.nextTick(function(){
+				$.ajax({
+					type: "GET",
+					data: data,
+					url: "/weather",
+					contentType: "application/json",
+					success: function(returnData) {
+						if (returnData.error !== undefined) {
+							self.error = "Cannot find weather - location is not valid";
+						} else {
+							self.weather = returnData;
+							self.launch();
+							self.error = null;
+						}
+					},
+					error: function(jqXHR, textStatus, errorThrown) {
+						self.error = "Cannot find weather - server error";
+					}
+				});
+			});
+		},
+
+		// --------- VALIDATION METHODS BELOW ---------
+
+		// Validator for US zipcodes
+		isValidUSZip: function() {
+		   return /^\d{5}(-\d{4})?$/.test(this.zip);
+		},
+
+		// --------- CONVERSION METHODS BELOW ---------
+
+		// Convert kelvin to farenheit or celsius
+		convertKelvin: function(temp) {
+			switch(this.unit) {
+				case "F":
+					return Math.round( (temp * 9/5) - 459.67) + "\xB0" + "F";
+				case "C":
+					return Math.round(temp - 273.15) + "\xB0" + "C";
+				default: 
+					return Math.round(temp) + "\xB0" + "K"
+			}
+		},
+		// Calculate icon gradient based on current temperature
+		getTempClass: function(temp) {
+			// use fahrenheit to do comparisons
+			var cur_temp = Math.round( (temp * 9/5) - 459.67) ;
+			switch(true) {
+			    case cur_temp >  80:
+			        return "hot";
+			    case cur_temp >  60:
+			        return "warm";
+			    case cur_temp >  40:
+			        return "cool";
+			    // cold
+			    default:
+			        return "cold";
+			}
+		},
+		// Format the time
+		timestamp: function() {
+			var date = new Date();
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var ampm = hours >= 12 ? 'PM' : 'AM';
+			hours = hours % 12;
+			hours = hours ? hours : 12; // the hour '0' should be '12'
+			minutes = minutes < 10 ? '0'+minutes : minutes;
+			var strTime = hours + ':' + minutes + ' ' + ampm;
+			return strTime;
+		},
+
+
+		// --------- WEATHER TO TEXT METHODS BELOW ---------
+
+		howCloudy: function(){
+			var clouds = this.weather.clouds;
+			switch(true) {
+			    case clouds >  80:
+			        return "It is very cloudy.";
+			    case clouds >  50:
+			        return "Skies are cloudy.";
+			    case clouds >  10:
+			        return "There are scattered clouds.";
+			    case clouds >  5:
+			        return "There are almost clear skies.";
+			    default:
+			        return "";
+			}
+		},
+		howWindy: function(){
+			var wind = this.weather.wind.speed;
+			switch(true) {
+			    case wind >  20:
+			        return "Be cautious of severe wind!";
+			    case wind >  11:
+			        return "There are strong winds.";
+			    case wind >  5:
+			        return "There are moderate winds.";
+			    case wind >  2:
+			        return "There is a light breeze.";
+			    default:
+			        return "";
+			}
+		},
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
